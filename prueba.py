fbb5b6535f8d84f713d9cfb2a28961fe93710389
pereman2 <pere-altea@hotmail.com>
2020-02-20 18:12:00 +0100

    xd

---
+++ prueba.py
@@ -0,0 +1,13 @@
+def get_input_line():
+    file_input = []
+    with open('a_example.in', 'r') as f:
+        
+        file_input = f.readlines()
+
+    return file_input
+
+
+inp = get_input_line()
+
+
+
